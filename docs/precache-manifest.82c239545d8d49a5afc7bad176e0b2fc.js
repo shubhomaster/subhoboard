922e16dcabf9b0de3c2d0fc3ee25a581d860a451
Shubhodeep Karmakar <59827772+shubhomaster@users.noreply.github.com>
2020-10-04 05:55:43 +0530

    Add files via upload

---
+++ docs/precache-manifest.82c239545d8d49a5afc7bad176e0b2fc.js
@@ -0,0 +1,64 @@
+self.__precacheManifest = [
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/XBoard/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/XBoard/robots.txt"
+  },
+  {
+    "revision": "311f124c951d90859346633e1a077d5c",
+    "url": "/XBoard/fonts/iconfont.311f124c.eot"
+  },
+  {
+    "revision": "92333a48c5b7f5ac8c5f",
+    "url": "/XBoard/css/app.40690573.css"
+  },
+  {
+    "revision": "63387bbfd9ea93860353",
+    "url": "/XBoard/css/chunk-vendors.239019ff.css"
+  },
+  {
+    "revision": "1dcfad1d03b6dbebbecf3b9177d47dde",
+    "url": "/XBoard/favicon.png"
+  },
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/XBoard/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/XBoard/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "7514c4d8f66fe9f9d6a375e549c8ce1f",
+    "url": "/XBoard/index.html"
+  },
+  {
+    "revision": "92333a48c5b7f5ac8c5f",
+    "url": "/XBoard/js/app.beebad5b.js"
+  },
+  {
+    "revision": "4d05c8777042af0915b3cf5fd9cf9308",
+    "url": "/XBoard/fonts/iconfont.4d05c877.ttf"
+  },
+  {
+    "revision": "99c79cd846788271049c058c991e1ede",
+    "url": "/XBoard/img/iconfont.99c79cd8.svg"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/XBoard/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "27e9de4053d6be5265ff",
+    "url": "/XBoard/css/Board.8a8a479b.css"
+  },
+  {
+    "url": "/XBoard/chunk-vendors.63387bbfd9ea93860353.js"
+  },
+  {
+    "url": "/XBoard/Board.27e9de4053d6be5265ff.js"
+  }
+];
